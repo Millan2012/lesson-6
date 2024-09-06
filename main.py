f6b1cc8ef8d0e3742ae62f63b9a6b29d8239cbdf
Millan2012 <millan.ramnathsing@gmail.com>
2024-09-06 17:58:32 +0200

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pgzrun
 from random import randint
 WIDTH=800
 HEIGHT=600
+next=0
+lines=[]
 option=int(input("1=easy,2=medium,3=hard"))
 if option==1:
     total=5
@@ -11,7 +13,7 @@ elif option==3:
     total=15
 sats=[]
 for i in range (total):
-
+    
     sat=Actor("satellite")
 
     sat.x=randint(50,WIDTH-50)
@@ -25,4 +27,21 @@ def draw():
         sat.draw()
         screen.draw.text(str(num),(sat.x,sat.y+35))
         num=num+1
+    for line in lines:
+        screen.draw.line(line[0],line[1],(255,255,255))
+
+
+def on_mouse_down(pos):
+    global next, lines 
+    if next < total:
+
+        if sats[next].collidepoint(pos):
+            if next:
+
+                lines.append((sats[next-1].pos,sats[next].pos))
+                #print(lines)
+            next+=1
+        else:
+            next=0
+            lines=[]
 pgzrun.go()
